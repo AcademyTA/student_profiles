b61a20130ac1975221092275e7c11d79f7ca6f2b
Wei Yi <weiyi2017@gmail.com>
2016-07-08 20:44:29 -0700

    first

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'https://rubygems.org'
 
-
+gem 'simple_form'
+gem "bootstrap-sass"
+gem "quiet_assets"
+gem 'carrierwave'
+gem "mini_magick"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use postgresql as the database for Active Record
@@ -43,7 +47,10 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
+  gem 'interactive_editor'
+  gem 'awesome_print'
+  gem 'hirb'
+  gem "fog"
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709004706) do
+ActiveRecord::Schema.define(version: 20160709010539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,5 +26,15 @@ ActiveRecord::Schema.define(version: 20160709004706) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "logo"
+    t.string   "web_app_link"
+    t.string   "github_link"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
